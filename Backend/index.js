b558665d28ff812883a38360c1a3791f9a43f4ec
Lars Drescher <larsvalentindrescher@gmail.com>
2023-04-27 17:56:04 +0200

    Backend fast fertig

--- Backend/index.js
+++ Backend/index.js
@@ -1,44 +1,80 @@
 import express from "express";
 import PocketBase from "pocketbase";
-import axios from "axios";
+import schedule from "node-schedule";
+import * as dotenv from "dotenv";
+
 const pb = new PocketBase("http://127.0.0.1:8090");
 const app = express();
-const port = 3000;
+app.set("view engine", "ejs");
 
-const waterSensorIp = "http://192.168.4.1:80";
+dotenv.config();
+
+const devMode = process.env.DEV_MODE;
+const port = process.env.PORT || 4040;
+const waterSensorIp = process.env.SENSOR_IP;
+
+/**
+ * Array mit Objekten
+ */
+let data = [];
 
 async function getData() {
+  if (devMode) {
+    const randomNumber = Math.floor(Math.random() * 100);
+    const randomData = {
+      waterLevel: randomNumber,
+      timeStamp: Date.now(),
+    };
+
+    data.push(randomData);
+    console.log(data);
+    await pb.collection("waterLevels").create(randomData);
+    return;
+  }
+
   try {
-    const response = await axios.get(waterSensorIp);
-    console.log(response.data);
+    const response = await fetch(waterSensorIp);
+    const resJson = await response.json();
+    const data = {
+      waterLevel: resJson.waterLevel,
+      timeStamp: Date.now(),
+    };
+
+    data.push(data);
+
+    await pb.collection("waterLevels").create(data);
+
+    return;
   } catch (error) {
     console.error(error);
   }
-
-  //await saveData(response.)
 }
 
-async function saveData(waterLevel) {
-  const data = {
-    waterLevel: waterLevel,
-    time: Date.now(),
-  };
-
-  const record = await pb.collection("waterLevels").create(data);
-}
-
-app.get("/", (req, res) => {
-  getData();
-  res.send("Hello World!");
+/**
+ * Standart Route
+ */
+app.get("/", async (req, res) => {
+  res.render("main", { data: data });
 });
 
-app.get("/config", (req, res) => {
-  getData();
-  res.send("config");
+app.get("/newdata/", async (req, res) => {
+  const timeStamp = req.query.timestamp;
+
+  if (!timeStamp) res.status(200).send(data);
 });
 
-// setInterval(getData(), 500);
+/**
+ * Führt einen Cron Job, der die Daten speichert in regelmäßigen Abständen aus
+ */
+const job = schedule.scheduleJob("*/5 * * * * *", async function () {
+  getData().catch((e) => {
+    console.log("Error Saving Data");
+  });
+});
 
+/**
+ * Initialisierung vom Web Server
+ */
 app.listen(port, () => {
   console.log(`Backend listening on port ${port}`);
 });

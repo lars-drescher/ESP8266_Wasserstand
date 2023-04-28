15d8f8c0fc4b536737cbde9c5a89af94756b82c4
Lars Drescher <larsvalentindrescher@gmail.com>
2023-04-28 13:26:37 +0200

    Finished Backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import PocketBase from "pocketbase";
 import schedule from "node-schedule";
 import * as dotenv from "dotenv";
+import { appendFileSync } from "fs";
 
 const pb = new PocketBase("http://127.0.0.1:8090");
 const app = express();
@@ -16,59 +17,69 @@ const waterSensorIp = process.env.SENSOR_IP;
 /**
  * Array mit Objekten
  */
-let data = [];
+let dataArray = [];
+
+function saveCsv(time, value) {
+  const csv = `${time},${value}\n`;
+  try {
+    appendFileSync("./log.csv", csv);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 async function getData() {
+  const data = {
+    waterLevel: 0,
+    timeStamp: Date.now(),
+  };
+
   if (devMode) {
     const randomNumber = Math.floor(Math.random() * 100);
-    const randomData = {
-      waterLevel: randomNumber,
-      timeStamp: Date.now(),
-    };
-
-    data.push(randomData);
-    console.log(data);
-    await pb.collection("waterLevels").create(randomData);
-    return;
+    data.waterLevel = randomNumber;
+  } else {
+    try {
+      const response = await fetch(waterSensorIp);
+      const resJson = await response.json();
+      data.waterLevel = resJson.waterLevel;
+    } catch (e) {
+      console.error(e);
+    }
   }
 
-  try {
-    const response = await fetch(waterSensorIp);
-    const resJson = await response.json();
-    const data = {
-      waterLevel: resJson.waterLevel,
-      timeStamp: Date.now(),
-    };
-
-    data.push(data);
-
-    await pb.collection("waterLevels").create(data);
-
-    return;
-  } catch (error) {
-    console.error(error);
-  }
+  // Save Data
+  console.log(data);
+  dataArray.push(data);
+  saveCsv(data.timeStamp, data.waterLevel);
+  //await pb.collection("waterLevels").create(data);
 }
 
 /**
  * Standart Route
  */
 app.get("/", async (req, res) => {
-  res.render("main", { data: data });
+  res.render("main");
 });
 
 app.get("/newdata/", async (req, res) => {
   const timeStamp = req.query.timestamp;
 
-  if (!timeStamp) res.status(200).send(data);
+  if (!timeStamp) return res.status(200).send(dataArray);
+
+  const arrayStartsAt = dataArray.findIndex(
+    (data) => data.timeStamp >= timeStamp
+  );
+
+  const newArray = dataArray.slice(arrayStartsAt + 1);
+  return res.status(200).send(newArray);
 });
 
 /**
  * Führt einen Cron Job, der die Daten speichert in regelmäßigen Abständen aus
  */
-const job = schedule.scheduleJob("*/5 * * * * *", async function () {
+const job = schedule.scheduleJob("*/2 * * * * *", async function () {
   getData().catch((e) => {
-    console.log("Error Saving Data");
+    console.log("Error Saving Data", e);
   });
 });
 
